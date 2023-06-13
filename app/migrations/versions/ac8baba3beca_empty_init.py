e57ff552156072b5bc02b39aa429adf2c7a71148
iamkipruto <kelvin.kipruto@student.moringaschool.com>
2023-06-13 14:07:02 +0300

    Alembic ini

---
+++ app/migrations/versions/ac8baba3beca_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: ac8baba3beca
+Revises: 
+Create Date: 2023-06-13 13:47:52.112912
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ac8baba3beca'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
